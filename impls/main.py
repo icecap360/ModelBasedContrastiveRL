89eaa1f05db848f6f81cd8a60fd0043fd9f0a59a
icecap360 <muhammad.qasim.ali06@gmail.com>
2025-08-19 12:58:01 -0400

    Using EMA updates

--- impls/main.py
+++ impls/main.py
@@ -3,9 +3,10 @@ import os
 import random
 import time
 from collections import defaultdict
-import jax.numpy as jnp
+from datetime import datetime
 
 import jax
+import jax.numpy as jnp
 import numpy as np
 import tqdm
 import wandb
@@ -16,62 +17,77 @@ from utils.datasets import Dataset, GCDataset, HGCDataset
 from utils.env_utils import make_env_and_datasets
 from utils.evaluation import evaluate
 from utils.flax_utils import restore_agent, save_agent
-from utils.log_utils import CsvLogger, get_exp_name, get_flag_dict, get_wandb_video, setup_wandb
-from datetime import datetime
+from utils.log_utils import (
+    CsvLogger,
+    get_exp_name,
+    get_flag_dict,
+    get_wandb_video,
+    setup_wandb,
+)
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('run_group', 'Debug', 'Run group.')
-flags.DEFINE_integer('seed', 0, 'Random seed.')
-flags.DEFINE_string('env_name', 'antmaze-large-navigate-v0', 'Environment (dataset) name.')
-flags.DEFINE_string('save_dir', 'results/', 'Save directory.')
-flags.DEFINE_string('dataset_dir', 'datasets/', 'Dataset directory.')
-flags.DEFINE_string('restore_path', None, 'Restore path.')
-flags.DEFINE_string('algo_name', None, 'Algorithm file name')
-flags.DEFINE_integer('restore_epoch', None, 'Restore epoch.')
+flags.DEFINE_string("run_group", "Debug", "Run group.")
+flags.DEFINE_integer("seed", 0, "Random seed.")
+flags.DEFINE_string(
+    "env_name", "antmaze-large-navigate-v0", "Environment (dataset) name."
+)
+flags.DEFINE_string("save_dir", "results/", "Save directory.")
+flags.DEFINE_string("dataset_dir", "datasets/", "Dataset directory.")
+flags.DEFINE_string("restore_path", None, "Restore path.")
+flags.DEFINE_string("algo_name", None, "Algorithm file name")
+flags.DEFINE_integer("restore_epoch", None, "Restore epoch.")
 
-flags.DEFINE_integer('train_steps', 1000000, 'Number of training steps.')
-flags.DEFINE_integer('log_interval', 1000,'Logging interval')
-flags.DEFINE_integer('eval_interval', 200000, 'Evaluation interval.') # 100000
-flags.DEFINE_integer('save_interval', 1000000, 'Saving interval.')
+flags.DEFINE_integer("train_steps", 1000000, "Number of training steps.")
+flags.DEFINE_integer("log_interval", 1000, "Logging interval")
+flags.DEFINE_integer("eval_interval", 200000, "Evaluation interval.")  # 100000
+flags.DEFINE_integer("save_interval", 1000000, "Saving interval.")
 
-flags.DEFINE_integer('eval_tasks', None, 'Number of tasks to evaluate (None for all).')
-flags.DEFINE_integer('eval_episodes', 20, 'Number of episodes for each task.')
-flags.DEFINE_float('eval_temperature', 0, 'Actor temperature for evaluation.')
-flags.DEFINE_float('eval_gaussian', None, 'Action Gaussian noise for evaluation.')
-flags.DEFINE_integer('video_episodes', 1, 'Number of video episodes for each task.')
-flags.DEFINE_integer('video_frame_skip', 3, 'Frame skip for videos.')
-flags.DEFINE_integer('eval_on_cpu', 0, 'Whether to evaluate on CPU.')
-flags.DEFINE_integer('frame_stack', 0, 'Number of frames to.')
-flags.DEFINE_float('alpha', 0.1, 'Actor\'s alpha value.')
+flags.DEFINE_integer("eval_tasks", None, "Number of tasks to evaluate (None for all).")
+flags.DEFINE_integer("eval_episodes", 20, "Number of episodes for each task.")
+flags.DEFINE_float("eval_temperature", 0, "Actor temperature for evaluation.")
+flags.DEFINE_float("eval_gaussian", None, "Action Gaussian noise for evaluation.")
+flags.DEFINE_integer("video_episodes", 1, "Number of video episodes for each task.")
+flags.DEFINE_integer("video_frame_skip", 3, "Frame skip for videos.")
+flags.DEFINE_integer("eval_on_cpu", 0, "Whether to evaluate on CPU.")
+flags.DEFINE_integer("frame_stack", 0, "Number of frames to.")
+flags.DEFINE_float("alpha", 0.1, "Actor's alpha value.")
 
 
 def main(_):
     # Set up logger.
     now = datetime.now()
     formatted_datetime = now.strftime("%d-%B %H-%M-%S")
-    config_flags.DEFINE_config_file('agent', f'impls/agents/{FLAGS.algo_name}.py', lock_config=False)
-    exp_name = f"{FLAGS.run_group} {formatted_datetime} {FLAGS.algo_name} {FLAGS.env_name} seed-{FLAGS.seed}" # get_exp_name(FLAGS.seed)
+    config_flags.DEFINE_config_file(
+        "agent", f"impls/agents/{FLAGS.algo_name}.py", lock_config=False
+    )
+    exp_name = f"{FLAGS.run_group} {formatted_datetime} {FLAGS.algo_name} {FLAGS.env_name} seed-{FLAGS.seed}"  # get_exp_name(FLAGS.seed)
     wandb.login(key="b5353c8c9266cff861ec00110c054264a62de918", relogin=True)
 
-    setup_wandb(project='OGBench', group=FLAGS.run_group, name=exp_name)
+    setup_wandb(project="OGBench", group=FLAGS.run_group, name=exp_name)
 
-    FLAGS.save_dir = os.path.join(FLAGS.save_dir, wandb.run.project, FLAGS.run_group, exp_name)
+    FLAGS.save_dir = os.path.join(
+        FLAGS.save_dir, wandb.run.project, FLAGS.run_group, exp_name
+    )
     os.makedirs(FLAGS.save_dir, exist_ok=True)
     flag_dict = get_flag_dict()
-    with open(os.path.join(FLAGS.save_dir, 'flags.json'), 'w') as f:
+    with open(os.path.join(FLAGS.save_dir, "flags.json"), "w") as f:
         json.dump(flag_dict, f)
 
     # Set up environment and dataset.
     config = FLAGS.agent
-    config['frame_stack'] = FLAGS.frame_stack if FLAGS.frame_stack > 0 else None # Override from CLI
-    config['alpha'] = FLAGS.alpha
-    env, train_dataset, val_dataset = make_env_and_datasets(FLAGS.env_name, frame_stack=None, dataset_dir=FLAGS.dataset_dir) # frame_stack=config['frame_stack']
+    config["frame_stack"] = (
+        FLAGS.frame_stack if FLAGS.frame_stack > 0 else None
+    )  # Override from CLI
+    config["alpha"] = FLAGS.alpha
+    env, train_dataset, val_dataset = make_env_and_datasets(
+        FLAGS.env_name, frame_stack=None, dataset_dir=FLAGS.dataset_dir
+    )  # frame_stack=config['frame_stack']
 
     dataset_class = {
-        'GCDataset': GCDataset,
-        'HGCDataset': HGCDataset,
-    }[config['dataset_class']]
+        "GCDataset": GCDataset,
+        "HGCDataset": HGCDataset,
+    }[config["dataset_class"]]
     train_dataset = dataset_class(Dataset.create(**train_dataset), config)
     if val_dataset is not None:
         val_dataset = dataset_class(Dataset.create(**val_dataset), config)
@@ -81,17 +97,19 @@ def main(_):
     np.random.seed(FLAGS.seed)
 
     example_batch = train_dataset.sample(1)
-    if config['discrete']:
+    if config["discrete"]:
         # Fill with the maximum action to let the agent know the action space size.
-        example_batch['actions'] = np.full_like(example_batch['actions'], env.action_space.n - 1)
+        example_batch["actions"] = np.full_like(
+            example_batch["actions"], env.action_space.n - 1
+        )
 
-    agent_class = agents[config['agent_name']]
+    agent_class = agents[config["agent_name"]]
     agent = agent_class.create(
         FLAGS.seed,
-        example_batch['observations'],
-        example_batch['actions'],
+        example_batch["observations"],
+        example_batch["actions"],
         config,
-        ex_batch=example_batch
+        ex_batch=example_batch,
     )
 
     # Restore agent.
@@ -99,11 +117,11 @@ def main(_):
         agent = restore_agent(agent, FLAGS.restore_path, FLAGS.restore_epoch)
 
     # Train agent.
-    train_logger = CsvLogger(os.path.join(FLAGS.save_dir, 'train.csv'))
-    eval_logger = CsvLogger(os.path.join(FLAGS.save_dir, 'eval.csv'))
+    train_logger = CsvLogger(os.path.join(FLAGS.save_dir, "train.csv"))
+    eval_logger = CsvLogger(os.path.join(FLAGS.save_dir, "eval.csv"))
     first_time = time.time()
     last_time = time.time()
-    
+
     progress_bar = tqdm.tqdm(total=FLAGS.train_steps, smoothing=0.1, dynamic_ncols=True)
     i = 0
     batch_queue = []
@@ -116,23 +134,22 @@ def main(_):
         # i += 1
 
         # Update agent.
-        batch = train_dataset.sample(config['batch_size'])
+        batch = train_dataset.sample(config["batch_size"])
         update_info = {}
         agent, update_info = agent.update_encoder(batch, i)
-        
+
         if i > warmup:
-            agent, rl_info = agent.update(batch, step=i-warmup)
+            agent, rl_info = agent.update(batch, step=i - warmup)
             update_info.update(rl_info)
-            if i % 500 == 0:
-                pass
-                agent = agent.update_encoder_target_hard(i)
+            # if i % 500 == 0:
+            #     pass
+            agent = agent.update_encoder_target_soft(i)
         else:
-            agent = agent.update_encoder_target_hard(i)
+            agent = agent.update_encoder_target_soft(i)
 
         progress_bar.update(1)
         i += 1
 
-        
         # Update agent.
         # update_info = {}
         # if i % 500 == 0 :
@@ -161,48 +178,68 @@ def main(_):
         #     update_info.update(encoder_update_info)
         # Log metrics.
         if i % FLAGS.log_interval == 0:
-            train_metrics = {f'training/{k}': v for k, v in update_info.items()}
+            train_metrics = {f"training/{k}": v for k, v in update_info.items()}
             if val_dataset is not None:
-                val_batch = val_dataset.sample(config['batch_size'])
+                val_batch = val_dataset.sample(config["batch_size"])
 
-                batch_size = val_batch['observations'].shape[0]
-                val_batch['observations'] = jnp.reshape(val_batch['observations'], (batch_size,-1))
-                val_batch['next_observations'] = jnp.reshape(batch['next_observations'], (batch_size,-1))
-                val_batch['actions'] = jnp.reshape(val_batch['actions'], (batch_size,-1))
+                batch_size = val_batch["observations"].shape[0]
+                val_batch["observations"] = jnp.reshape(
+                    val_batch["observations"], (batch_size, -1)
+                )
+                val_batch["next_observations"] = jnp.reshape(
+                    batch["next_observations"], (batch_size, -1)
+                )
+                val_batch["actions"] = jnp.reshape(
+                    val_batch["actions"], (batch_size, -1)
+                )
 
-                _, encoder_info = agent._encoder_loss_fn_for_grad(agent.encoder.params, val_batch, i)
-                encoder_info.pop('current_batch_avg_teacher_logits', None)
+                _, encoder_info = agent._encoder_loss_fn_for_grad(
+                    agent.encoder.params, val_batch, i
+                )
+                encoder_info.pop("current_batch_avg_teacher_logits", None)
                 new_info = {}
-                for k,v in encoder_info.items():
-                    if k.startswith('encoder_'):
+                for k, v in encoder_info.items():
+                    if k.startswith("encoder_"):
                         new_info[k] = v
                     else:
-                        new_info[f'encoder_{k}'] = v
+                        new_info[f"encoder_{k}"] = v
                 encoder_info = new_info
 
                 _, val_info = agent.total_loss(val_batch, grad_params=None)
                 val_info.update(encoder_info)
 
-                train_metrics.update({f'validation/{k}': v for k, v in val_info.items()})
-            train_metrics['time/epoch_time'] = (time.time() - last_time) / FLAGS.log_interval
-            train_metrics['time/total_time'] = time.time() - first_time
+                train_metrics.update(
+                    {f"validation/{k}": v for k, v in val_info.items()}
+                )
+            train_metrics["time/epoch_time"] = (
+                time.time() - last_time
+            ) / FLAGS.log_interval
+            train_metrics["time/total_time"] = time.time() - first_time
             last_time = time.time()
             wandb.log(train_metrics, step=i)
             train_logger.log(train_metrics, step=i)
 
         # Evaluate agent.
-        if i % FLAGS.eval_interval == 0: # or i == 1 
+        if i % FLAGS.eval_interval == 0:  # or i == 1
             if FLAGS.eval_on_cpu:
-                eval_agent = jax.device_put(agent, device=jax.devices('cpu')[0])
+                eval_agent = jax.device_put(agent, device=jax.devices("cpu")[0])
             else:
-                eval_agent = jax.device_put(agent, device=jax.devices("gpu")[0]) # agent
+                eval_agent = jax.device_put(
+                    agent, device=jax.devices("gpu")[0]
+                )  # agent
             renders = []
             eval_metrics = {}
             overall_metrics = defaultdict(list)
-            task_infos = env.unwrapped.task_infos if hasattr(env.unwrapped, 'task_infos') else env.task_infos
-            num_tasks = FLAGS.eval_tasks if FLAGS.eval_tasks is not None else len(task_infos)
+            task_infos = (
+                env.unwrapped.task_infos
+                if hasattr(env.unwrapped, "task_infos")
+                else env.task_infos
+            )
+            num_tasks = (
+                FLAGS.eval_tasks if FLAGS.eval_tasks is not None else len(task_infos)
+            )
             for task_id in tqdm.trange(1, num_tasks + 1):
-                task_name = task_infos[task_id - 1]['task_name']
+                task_name = task_infos[task_id - 1]["task_name"]
                 eval_info, trajs, cur_renders = evaluate(
                     agent=eval_agent,
                     env=env,
@@ -215,19 +252,23 @@ def main(_):
                     eval_gaussian=FLAGS.eval_gaussian,
                 )
                 renders.extend(cur_renders)
-                metric_names = ['success']
+                metric_names = ["success"]
                 eval_metrics.update(
-                    {f'evaluation/{task_name}_{k}': v for k, v in eval_info.items() if k in metric_names}
+                    {
+                        f"evaluation/{task_name}_{k}": v
+                        for k, v in eval_info.items()
+                        if k in metric_names
+                    }
                 )
                 for k, v in eval_info.items():
                     if k in metric_names:
                         overall_metrics[k].append(v)
             for k, v in overall_metrics.items():
-                eval_metrics[f'evaluation/overall_{k}'] = np.mean(v)
+                eval_metrics[f"evaluation/overall_{k}"] = np.mean(v)
 
             if FLAGS.video_episodes > 0:
                 video = get_wandb_video(renders=renders, n_cols=num_tasks)
-                eval_metrics['video'] = video
+                eval_metrics["video"] = video
 
             wandb.log(eval_metrics, step=i)
             eval_logger.log(eval_metrics, step=i)
@@ -240,5 +281,5 @@ def main(_):
     eval_logger.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(main)
